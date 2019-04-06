22e561f9aa478571d872380908aa5c5e52b69cae
mmcmill <mmcmill@ncsu.edu>
2019-04-06 04:48:38 -0400

    things class implemented

--- 2a/src/dsl/thingclasses.java
+++ 2a/src/dsl/thingclasses.java
@@ -1,119 +1,133 @@
-abstract class Thing {
-  int init = 0;
-  int lo = 0;
-  int hi = 100;
-  String txt = "thing";
-  
-  Thing(int init, int lo, int hi, String txt) {
-     if (init > 0) {
-      this.init = init;
-    } else if (lo > 0){
-      this.init = lo;
+abstract class Thing implements Comparable<Thing> {
+    int    init = 0;
+    int    lo   = 0;
+    int    hi   = 100;
+    String txt  = "thing";
+
+    Thing ( final int init, final int lo, final int hi, final String txt ) {
+        if ( init > 0 ) {
+            this.init = init;
+        }
+        else if ( lo > 0 ) {
+            this.init = lo;
+        }
+
+        setLo( lo );
+        setHi( hi );
+        setTxt( txt );
+
     }
-    
-    setLo(lo);
-    setHi(hi);
-    setTxt(txt);
-    
-  }
-  
-  void setLo(int lo){
-    if (lo < 0) {
-      throw new IllegalArgumentException("lo cannot be negative");
+
+    void setLo ( final int lo ) {
+        if ( lo < 0 ) {
+            throw new IllegalArgumentException( "lo cannot be negative" );
+        }
+        // zero is default value.
+        if ( lo == 0 ) {
+            return;
+        }
+
+        this.lo = lo;
+        if ( this.init >= this.lo ) {
+            this.lo = this.init;
+        }
     }
-    // zero is default value.
-    if (lo == 0) { 
-      return;
+
+    void setHi ( final int hi ) {
+        if ( hi < 0 ) {
+            throw new IllegalArgumentException( "hi cannot be negative" );
+        }
+
+        // zero is default value
+        if ( hi == 0 ) {
+            return;
+        }
+
+        this.hi = hi;
+        if ( this.init >= this.hi ) {
+            this.lo = this.init * 2;
+        }
     }
-    
-    this.lo = lo;
-    if(this.init >= this.lo) {
-      this.lo = this.init;
+
+    void setTxt ( final String txt ) {
+        if ( txt != null ) {
+            this.txt = txt;
+        }
     }
-  }
-  
-  void setHi(int hi){
-    if (hi < 0) {
-      throw new IllegalArgumentException("hi cannot be negative");
+
+    int restrain ( final int x ) {
+        return Math.max( this.lo, Math.min( this.hi, x ) );
     }
-    
-   // zero is default value
-    if (hi == 0) { 
-      return;
+
+    abstract int rank ();
+
+    @Override
+    public int compare ( final Thing o1, final Thing o2 ) {
+        if ( o1.rank() < o2.rank() ) {
+            return -1;
+        }
+        else if ( o1.rank() > o2.rank() ) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
     }
-    
-    this.hi = hi;
-    if(this.init >= this.hi) {
-      this.lo = this.init*2;
-    }
-  }
-  
-  void setTxt(String txt) {
-    if( txt != null) {
-      this.txt = txt;
-    }
-  }
-  
-  int restrain(int x){
-    return Math.max(this.lo, Math.min(this.hi, x));
-  }
-  
-  abstract int rank();
-  
 }
 
 class Percent extends Thing {
 
     static int PERCENT_RANK = 4;
-    
-    Percent ( int init, int lo, int hi, String txt ) {
+
+    Percent ( final int init, final int lo, final int hi, final String txt ) {
         super( init, lo, hi, txt );
     }
 
     @Override
     int rank () {
         return PERCENT_RANK;
-    }  
+    }
+
 }
 
 class Flow extends Thing {
 
     static int FLOW_RANK = 3;
-    
-    Flow ( int init, int lo, int hi, String txt ) {
+
+    Flow ( final int init, final int lo, final int hi, final String txt ) {
         super( init, lo, hi, txt );
     }
 
     @Override
     int rank () {
         return FLOW_RANK;
-    }  
+    }
 }
 
 class Stock extends Thing {
 
     static int STOCK_RANK = 2;
-    
-    Stock ( int init, int lo, int hi, String txt ) {
+
+    Stock ( final int init, final int lo, final int hi, final String txt ) {
         super( init, lo, hi, txt );
     }
 
     @Override
     int rank () {
         return STOCK_RANK;
-    }  
+    }
 }
 
 class Aux extends Thing {
 
     static int AUX_RANK = 1;
-    
-    Aux ( int init, int lo, int hi, String txt ) {
+
+    Aux ( final int init, final int lo, final int hi, final String txt ) {
         super( init, lo, hi, txt );
     }
 
     @Override
     int rank () {
         return AUX_RANK;
-    }  
+    }
 }
---
+++ 2a/src/dsl/things.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class Things {
+
+    // key is the name of the thing
+    HashMap<String, Thing> things;
+    // String is the name of the things in order by the rank of the thing
+    ArrayList<String>      order;
+
+    Things ( final HashMap<String, Thing> things ) {
+        if ( things == null ) {
+            throw new IllegalArgumentException( "Things cannot be null" );
+        }
+        this.things = things;
+
+        // sort the things by rank, using comparator in Thing
+        final SortedSet<Thing> sortedByRank = new TreeSet<Thing>( this.things.values() );
+
+        for ( final Thing value : sortedByRank ) {
+            order.add( getKey( value ) );
+        }
+
+        // set the name for the things to be the keys
+        for ( final String key : this.order ) {
+            this.things.get( key ).setTxt( key );
+        }
+    }
+
+    private String getKey ( final Thing value ) {
+        for ( final Entry<String, Thing> entry : this.things.entrySet() ) {
+            if ( entry.getValue().equals( value ) ) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    HashMap<String, Integer> payload ( final HashMap<String, Integer> old ) {
+        final HashMap<String, Integer> out = new HashMap<String, Integer>();
+        for ( final String key : this.order ) {
+            out.put( key, this.things.get( key ).init );
+        }
+
+        if ( old != null ) {
+            for ( final Entry<String, Integer> oldEntry : old.entrySet() ) {
+                final String key = oldEntry.getKey();
+
+                out.put( key, things.get( key ).restrain( old.get( key ) ) );
+            }
+        }
+
+        return out;
+
+    }
+
+    ArrayList<Thing> toList () {
+        final ArrayList<Thing> listThings = new ArrayList<Thing>();
+        for ( final String key : order ) {
+            listThings.add( things.get( key ) );
+        }
+        return listThings;
+    }
+}
